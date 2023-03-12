7edeeafec52007dc841338596f76bc2091f71e8c
iwant <53087594+1want@users.noreply.github.com>
2023-03-12 21:56:45 +0800

    添加全局请求拦截器

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { AuthService } from './auth.service'
 import { LocalStorage } from './local.strategy'
 import { JwtStorage } from './jwt.strategy'
-import { UserModule } from 'src/user/user.module'
-import { PassportModule } from '@nestjs/passport'
-import { JwtModule } from '@nestjs/jwt'
-import { jwtConstants } from './constants'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from '../user/entities/user.entity'
-// import { UserService } from 'src/user/user.service'
+import { jwtConstants } from './constants'
+import { UserService } from 'src/user/user.service'
 
 @Module({
   imports: [
-    UserModule,
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
@@ -20,7 +18,7 @@ import { User } from '../user/entities/user.entity'
     }),
     TypeOrmModule.forFeature([User])
   ],
-  providers: [AuthService, LocalStorage, JwtStorage],
-  exports: [AuthService, JwtModule]
+  providers: [AuthService, LocalStorage, JwtStorage, UserService],
+  exports: [AuthService]
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter'
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  // 注册全局错误的过滤器
+  app.useGlobalInterceptors(new TransformInterceptor())
+  // 全局注册拦截器
+  app.useGlobalFilters(new HttpExceptionFilter())
   const options = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
